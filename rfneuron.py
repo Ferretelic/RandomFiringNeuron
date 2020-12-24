a02b439148d7c75a6bfb90e1c995059fab8ddc88
Ferretelic <kyleshouki@gmail.com>
2020-12-24 23:38:53 +0900

    rf_neuron for pytorch

--- rfneuron.py
+++ rfneuron.py
@@ -1,11 +1,13 @@
 import numpy as np
+import torch
 
-class RandomFiringNeuron():
+class RandomFiringNeuron(torch.autograd.Function):
   def __init__(self, proportions=[0.5, 0.2], weights=[0.1, 1], gradient_proportions=[0.01, 0.01]):
     self.proportions = proportions
     self.weights = weights
     self.gradient_proportions = gradient_proportions
 
+  @staticmethod
   def forward(self, x):
     threshhold = np.random.randn(x.shape[0]) * self.proportions[0]
     self.activation = np.where(x <= threshhold, False, True)
@@ -16,6 +18,7 @@ class RandomFiringNeuron():
 
     return y
 
+  @staticmethod
   def backward(self, d_input):
     d_output = np.empty_like(d_input)
     d_output[np.logical_not(self.activation)] = self.weights[0] + np.random.randn(np.sum(np.logical_not(self.activation))) * self.gradient_proportions[0]
